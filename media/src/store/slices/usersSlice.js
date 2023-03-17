a62a5b20a42223603766e5a87d06c83769b3f10f
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-17 15:25:25 -0700

    added extraReducer to user slice file

--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
   name: 'users',
   initialState: {
-    data: []
+    data: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {}
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // update our state object to show user are loading
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // show user we finish the request
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      // show user request fail
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  }
 });
 
 export const usersReducer = usersSlice.reducer;
