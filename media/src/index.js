cd29d740990d8cfdcf5b4db6ac83234590ca8131
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-16 21:51:21 -0700

    Finish set up app files
    npm install react-redux

--- media/src/index.js
+++ media/src/index.js
@@ -1,9 +1,15 @@
 import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <provider >
+    <App />
+  </provider>
+);
