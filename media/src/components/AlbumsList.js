b17f737cdb473f785c6b4c79d8a1e6821548108d
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-19 12:14:51 -0700

    add in button to add more album using mutation

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -1,11 +1,48 @@
-import { useFetchAlbumsQuery } from '../store';
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } =useFetchAlbumsQuery(user);
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-  console.log(data, error, isLoading);
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
   
-  return <div> Albums for {user.name}</div>
+  let content; // if loading is true show skeleton
+  // also have to pass in a times prop for skeleton to show # boxes
+  if (isLoading) {
+    content = <Skeleton times={3}/>
+    // check if error if error display the div
+  } else if (error) {
+    content = <div> Error loading albums.</div>
+    // otherwise if not loading or no error mean we have data
+  } else {
+    // map over each album oject and for each one create expandable panel
+    // also make sure it has a header of album title
+    content = data.map(album => {
+      // header props
+      const header = <div>{album.title}</div>;
+    
+      return <ExpandablePanel key={album.id} header={header}>
+       List of photos in the album
+      </ExpandablePanel>
+    })
+  }
+
+
+  return (
+    <div> 
+      <div> Albums for {user.name} 
+        <Button onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
+      <div> {content} </div>
+    </div>
+  );
 }
 
 export default AlbumsList;
